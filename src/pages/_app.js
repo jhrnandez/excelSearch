4c9c85c3beb0c9bea9de2ee87f7c8cdf4e4158d9
Juan Manuel Henandez  Flores <juan@brandpetram.com>
2023-06-23 15:53:02 -0600

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-EJTBPNTR96"
+        strategy="lazyOnload"
+      />
+      <Script
+        id="gtag-init"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-EJTBPNTR96');
+        `,
+        }}
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
